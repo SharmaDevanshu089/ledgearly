a9f6fe4cbd9a07845abe63ae64751ad016037e02
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-12-06 12:00:12 +0000

    added Add Expesne

--- programs/ledgearly/src/lib.rs
+++ programs/ledgearly/src/lib.rs
@@ -10,6 +10,21 @@ pub mod ledgearly {
     list.expenses = Vec::new();
     Ok(())
 }
+pub fn add_expense(
+    ctx: Context<AddExpense>,
+    name: String,
+    cost: u64,
+) -> Result<()> {
+    let list = &mut ctx.accounts.expense_list;
+
+    // limit check
+    require!(list.expenses.len() < 10, ErrorCode::ListFull);
+
+    list.expenses.push(Expense { name, cost });
+
+    Ok(())
+}
+
 
     // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
     //     msg!("Greetings from: {:?}", ctx.program_id);
@@ -33,7 +48,7 @@ pub struct ExpenseList {
 #[account(
     init,
     payer = user,
-    space = 600, // safe for 10 expenses
+    space = 600, //todo change later
     seeds = [b"expense_list", user.key().as_ref()],
     bump
 )]
